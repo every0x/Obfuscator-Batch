bfff2f83defc3f24c0f2a2882260a3bac92efc2b
every0x <84487674+every0x@users.noreply.github.com>
2021-05-20 12:11:58 -0300

    Add files via upload

---
+++ Obfuscator Master/App.bat
@@ -0,0 +1,23 @@
+@echo off
+color 2
+echo.
+echo Hi
+echo.
+pause
+
+cls
+echo.
+echo this is an obfuscation
+echo.
+pause
+
+cls
+echo.                         
+echo  _       _____   _       
+echo (_)     (_____) (_)      
+echo (_)    (_)   (_)(_)      
+echo (_)    (_)   (_)(_)      
+echo (_)____(_)___(_)(_)____  
+echo (______)(_____) (______) 
+echo.                                                 
+pause
